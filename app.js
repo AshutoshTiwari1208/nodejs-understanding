e837d61091aa95006f707e3dda82f68468a948b7
ashutoshtiwari3309 <ashutosh.tiwari@squareboat.com>
2020-07-08 23:16:57 +0530

    Sending HTML Response,Response Header Set

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
-	res.setHeader("Content-Type", "text/html");
+	// console.log(req.url, req.method, req.headers); //Check data we got
+	res.setHeader("Content-Type", "text/html"); //set Headers
 
-	res.write("<html>");
+	res.write("<html>"); //Writing Data to Response Obj
 	res.write("<head><title>Inclined Scorpio</title></head>");
 	res.write("<body> Let's Start Node.js</body>");
 	res.write("</html>");
-	res.end();
+
+	res.end(); //Asking Node.js to Send The Response
 });
 
 server.listen(3000);
