a3b6fc01989ebb416ed28d34f306768f28921d09
ashutoshtiwari3309 <ashutosh.tiwari@squareboat.com>
2020-07-09 15:07:14 +0530

    Form Creation, Route Handelling, Redirect

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	// console.log(req.url, req.method, req.headers); //Check data we got
-	res.setHeader("Content-Type", "text/html"); //set Headers
+	if (req.url == "/") {
+		res.write("<html>");
+		res.write("<head><title>IS</title></head>");
+		res.write("<body>");
+		res.write(
+			"<form method='POST' action='/home'><input name='messsage'/><button type='submit'>Send</button></form>"
+		);
+		res.write("</body></html");
+		return res.end();
+	}
+	if (req.url == "/home") {
+		res.statusCode = 302; //redirect !
+		res.setHeader("location", "/"); //location to redirect
+		fs.writeFileSync("names.txt", "bhushan ji");
+		return res.end();
+	}
 
-	res.write("<html>"); //Writing Data to Response Obj
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
 	res.write("<head><title>Inclined Scorpio</title></head>");
 	res.write("<body> Let's Start Node.js</body>");
 	res.write("</html>");
 
-	res.end(); //Asking Node.js to Send The Response
+	res.end();
 });
 
 server.listen(3000);
